d9cb176e63f6ce52b9823a84dffcb9e1dd6093e0
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-03 11:23:24 +0530

    Simple C# console app (HelloWorld)

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 23;
+            Console.WriteLine("String formatting : {0}", number);
+        }
+    }
+}
