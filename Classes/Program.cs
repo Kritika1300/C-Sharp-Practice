dc8472162fa21e7f65d1f5a88057ff06ded5a025
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-04 10:36:18 +0530

    Summarize Text

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -2,32 +2,14 @@
 
 namespace Classes
 {
-  
+
     class Program
     {
         static void Main(string[] args)
         {
-            string fullName = "Kritika Kaur      ";
-            Console.WriteLine("My name is '{0}'",fullName.Trim());
-            Console.WriteLine("My name is '{0}'", fullName.Trim().ToUpper());
-
-            var index = fullName.IndexOf(' ');
-            var firstName = fullName.Substring(0, index);
-            var lastName = fullName.Substring(index + 1);
-            Console.WriteLine(firstName+ "\n" + lastName);
-
-            var arr = fullName.Split(' ');
-           
-            Console.WriteLine(arr[0] + arr[1]);
-           
-
-            int num = 1234;
-            string str = num.ToString("C0");
-            Console.WriteLine(str);
-
-
-
-
+            string sentence = "hi how are you this is a long long very long sentence which has to be shortened";
+            string summary = StringUtility.SummarizeText(sentence, 12);
+            Console.WriteLine(summary);
         }
     }
 
