e529ddadc0f88713c98a42c96d35430096b58585
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-05 18:53:43 +0530

    Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -8,15 +8,13 @@ namespace Classes
     {
         static void Main(string[] args)
         {
-            var path = @"C:\Users\Hp\Desktop\C#\C-Sharp-Practice\Classes\Classes.sln";
-            var extension = Path.GetExtension(path);
-            Console.WriteLine(extension);
-            var filename = Path.GetFileName(path);
-            Console.WriteLine(filename);
-            var file = Path.GetFileNameWithoutExtension(path);
-            Console.WriteLine(file);
-            var dir = Path.GetDirectoryName(path);
-            Console.WriteLine(dir);
+           int count = 0;
+            for(int i = 1; i <= 100; i++)
+            {
+                if(i % 3 == 0)
+                    count++;
+            }
+            Console.WriteLine(count);
         }
 
     }
