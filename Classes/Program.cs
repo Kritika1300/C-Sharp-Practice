30731dfacbd928d1761a46f2f47c5ea2ff157b3d
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-07 21:42:51 +0530

    Params keyword

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -5,27 +5,14 @@ namespace Classes {
 
     class Program
     {
-        public static int MaxNumber(int[] arr)
+        public static  int MaxNumber(params int[] arr)
         {
             return arr.Max();
         }
         static void Main(string[] args)
         {
-            int[][] arr = new int[2][];
-            arr[0] = new int[3] { 1, 2, 3 };
-            arr[1] = new int[2] { 1, 2 };
-
-            for(var i = 0; i < arr.Length; i++)
-            {
-                for(var j = 0; j < arr[i].Length; j++)
-                {
-                    Console.Write(arr[i][j] + " ");
-                }
-                Console.WriteLine();
-            }
-            
-       
-
+        
+            Console.WriteLine(MaxNumber(1,3,4,5,89,90,65));
 
         }
     }
