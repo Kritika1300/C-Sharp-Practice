55987b04c8385653c757dcdb06f6575c69285fbe
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-10 11:44:39 +0530

    Classes ad Objects

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -2,21 +2,33 @@
 
 namespace Classes
 {
+    class Employee
+    {
+        public static  string firstName;
+        public static string lastName;
+
+        public void FullName() //instance method
+        {
+            Console.WriteLine(firstName + " " + lastName);
+        }
+
+        public static void Greeting()
+        {
+            Console.WriteLine($"Hey {firstName} {lastName}");
+        }
+
+    }
     class Program
     {
-        public static void Calculate(int a, int b, out int[] sum, out int[] product)
-        {
-            sum = new int[3];
-            product = new int[3];
-            sum[0] = a + b;
-            product[0] = a * b;
-        }
+        
         static void Main(string[] args)
         {
-            int[] add;
-            int[] mul;
-            Calculate(2, 3, out add, out mul);
-            Console.WriteLine("Addition = {0}, Multiplication = {1}",add[0],mul[0]);
+            Employee emp1 = new Employee();
+            Employee.firstName = "Kritika";
+            Employee.lastName = "Kaur";
+            emp1.FullName();
+            Employee.Greeting();
+
         }
     }
 }
