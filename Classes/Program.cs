70bf45e7591107e34b3090c5fa27aa3aa251f2dd
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-10 17:16:55 +0530

    Iterating through enum values + names

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,35 +1,26 @@
 ﻿using System;
-public class Employee
-{
-    public string firstName;
-    public string lastName;
-    public string email;
-
-    public Employee()
-    {
-        Console.WriteLine("Base class constructor"); 
-    }
-    public Employee(string message)
-    {
-        Console.WriteLine(message);
-    }
-}
-class FullTimeEmployee : Employee {
-   
-    public FullTimeEmployee() : base("Derived class constructor controlling base class constructor")
-    {
-        Console.WriteLine("Child class constructor");
-    }
-
-}
-
 class Test
 {
+    public enum Days { 
+    Monday = 1,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+    }
+
     public static void Main(string[] args)
     {
-        Employee FTE = new FullTimeEmployee();
-        FTE.firstName = "Kritika";
-        FTE.lastName = "Kaur";
-        FTE.email = "k@gmail.com";
+        foreach(int i in Enum.GetValues(typeof(Days)))
+        {
+            Console.WriteLine(i);
+        }
+        foreach(string s in Enum.GetNames(typeof(Days)))
+        {
+            Console.WriteLine(s);
+        }
+        
     }
 }
