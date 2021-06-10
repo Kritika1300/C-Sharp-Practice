dce016e54443be0aef85f668361bf853d21440f2
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-10 18:04:56 +0530

    Strongly typed - Enums

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -10,13 +10,18 @@ class Test
     Saturday,
     Sunday
     }
+    public enum Season { 
+    Summer,
+    Winter,
+    Spring
+    }
+
 
     public static void Main(string[] args)
     {
-        foreach(int i in Enum.GetValues(typeof(Days)))
-        {
-            Console.WriteLine(i);
-        }
+        Days Funday = (Days)1;
+        int a = (int)Days.Monday;
+        Days WorkDay = (Days)Season.Summer;
         foreach(string s in Enum.GetNames(typeof(Days)))
         {
             Console.WriteLine(s);
