befea8cca71b0397c6ca4b5bb3b610ab60d45e5a
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-10 19:21:06 +0530

    Structures

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,31 +1,47 @@
 ﻿using System;
 class Test
 {
-    public enum Days { 
-    Monday = 1,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-    Saturday,
-    Sunday
-    }
-    public enum Season { 
-    Summer,
-    Winter,
-    Spring
-    }
+    public struct Customer {
 
+        private string _fname;
+        private string _lname;
+        
+        public string _Fname
+        {
+            get { return _fname; }
+            set { _fname = value; }
+        }
+        public string _Lname
+        {
+            get { return _lname; }
+            set { _lname = value; }
+        }
+        public Customer(string fname, string lname)
+        {
+            _fname = fname;
+            _lname = lname;
+        }
+        public void PrintDetails()
+        {
+            Console.WriteLine($"{_fname} {_lname}");
+        }
 
+    }
     public static void Main(string[] args)
     {
-        Days Funday = (Days)1;
-        int a = (int)Days.Monday;
-        Days WorkDay = (Days)Season.Summer;
-        foreach(string s in Enum.GetNames(typeof(Days)))
+        Customer c1 = new Customer("A", "B");
+        c1.PrintDetails();
+        Customer c2 = new Customer();
+        c2._Fname = "C";
+        c2._Lname = "D";
+        c2.PrintDetails();
+        Customer c3 = new Customer
         {
-            Console.WriteLine(s);
-        }
-        
+            _Fname = "E",
+            _Lname = "G"
+        };
+        c3.PrintDetails();
+
+
     }
 }
