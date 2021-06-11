bbae1e22bf7ad6180a60b1274219fc2fe96d828e
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-11 15:33:14 +0530

    ToPascalCase exercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,47 +1,24 @@
 ﻿using System;
-class Test
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+class Program
 {
-    public struct Customer {
-
-        private string _fname;
-        private string _lname;
-        
-        public string _Fname
-        {
-            get { return _fname; }
-            set { _fname = value; }
-        }
-        public string _Lname
-        {
-            get { return _lname; }
-            set { _lname = value; }
-        }
-        public Customer(string fname, string lname)
-        {
-            _fname = fname;
-            _lname = lname;
-        }
-        public void PrintDetails()
-        {
-            Console.WriteLine($"{_fname} {_lname}");
-        }
-
-    }
+    
     public static void Main(string[] args)
     {
-        Customer c1 = new Customer("A", "B");
-        c1.PrintDetails();
-        Customer c2 = new Customer();
-        c2._Fname = "C";
-        c2._Lname = "D";
-        c2.PrintDetails();
-        Customer c3 = new Customer
+        string[] input = Console.ReadLine().ToLower().Split(" ");
+        for (int i = 0; i < input.Length; i++)
         {
-            _Fname = "E",
-            _Lname = "G"
-        };
-        c3.PrintDetails();
+            var newword = input[i].Substring(0,1).ToUpper() + input[i].Substring(1);
+            input[i] = newword;
 
-
+        }
+        var newString = "";
+        foreach (var word in input)
+        {
+            newString += word;
+        }
+        Console.WriteLine(newString);
     }
 }
