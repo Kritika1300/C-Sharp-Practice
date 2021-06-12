72cc5587fd2abf0af8e87191d5363e74b5d90e27
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-12 17:14:37 +0530

    Use case of static method

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -2,27 +2,26 @@
 using System.Linq;
 using System.IO;
 using System.Collections.Generic;
-class Program
+class Person
 {
+    public string fname;
 
+    public void Introduce(string name)
+    {
+        Console.WriteLine("Hi {0}, I am {1}",fname,name);
+    }
+
+    public static Person Parse(string s)
+    {
+        var person = new Person();
+        person.fname = s;
+
+        return person;
+    } 
     public static void Main(string[] args)
     {
-
-        //array to string
-        string str = new string("HELOOOOOO");
-        int[] arr = { 1, 2, 3, 3 };
-        string s = string.Join(",",1233333,str,"pppppppppppppppppp"); //Method 1
-        string s1 = string.Join("",arr); //Method 2
-
-        char[] arr1 = { 'p', 'y' };
-        string s2 = new string(arr1); // Only for char array
-        Console.WriteLine(s);
-
-        string s3 = "Kritika";
-        char[] arr2 = s3.ToCharArray();
-        Console.WriteLine(arr2);
-
-
+        var p = Person.Parse("Krika");
+        p.Introduce("Lolo");
 
     } 
 }
