8d0984f53657166226842fb150606f1f23a43ad7
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-12 12:06:17 +0530

    Array to string + string to CharArray

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,21 +4,25 @@ using System.IO;
 using System.Collections.Generic;
 class Program
 {
-    
+
     public static void Main(string[] args)
     {
-        string[] input = Console.ReadLine().ToLower().Split(" ");
-        for (int i = 0; i < input.Length; i++)
-        {
-            var newword = input[i].Substring(0,1).ToUpper() + input[i].Substring(1);
-            input[i] = newword;
 
-        }
-        var newString = "";
-        foreach (var word in input)
-        {
-            newString += word;
-        }
-        Console.WriteLine(newString);
-    }
+        //array to string
+        string str = new string("HELOOOOOO");
+        int[] arr = { 1, 2, 3, 3 };
+        string s = string.Join(",",1233333,str,"pppppppppppppppppp"); //Method 1
+        string s1 = string.Join("",arr); //Method 2
+
+        char[] arr1 = { 'p', 'y' };
+        string s2 = new string(arr1); // Only for char array
+        Console.WriteLine(s);
+
+        string s3 = "Kritika";
+        char[] arr2 = s3.ToCharArray();
+        Console.WriteLine(arr2);
+
+
+
+    } 
 }
