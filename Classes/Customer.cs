779086516ee337377bad2b547a50a3cdcc42ece5
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-12 19:44:19 +0530

    Params keyword usecase

--- Classes/Customer.cs
+++ Classes/Customer.cs
@@ -4,22 +4,41 @@ namespace Classes
 {
     class Customer
     {
-        public string name;
-        public int id;
+        //public int Add(int[] numbers)
+        //{
+        //    int sum = 0;
+        //    foreach(var i in numbers)
+        //    {
+        //        sum += i;
+        //    }
+        //    return sum;
+        //}
+        //public static void Main()
+        //{
+        //    Customer c = new Customer();
+        //    Console.WriteLine(c.Add(new int[3] { 1, 2, 3 })); 
 
+        //}
+
+        //All of this can be avoided using params keyword
+
+        public int Add(params int[] numbers)
+        {
+            int sum = 0;
+            foreach (var i in numbers)
+            {
+                sum += i;
+            }
+            return sum;
+        }
         public static void Main()
         {
-            Customer c = new Customer
-            {
-                name = "Kritika",
-                id = 1
-            };
-
-            Console.WriteLine(c.id);
-            Console.WriteLine(c.name);
-
+            Customer c = new Customer();
+            Console.WriteLine(c.Add( 1, 2, 3, 4, 5 ));
 
         }
+
+
     }
 
 }
