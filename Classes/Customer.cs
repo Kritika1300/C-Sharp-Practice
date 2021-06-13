f5bae15d56e0465929d13eda4b9f112bb535fa6a
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-13 12:54:17 +0530

    Properties

--- Classes/Customer.cs
+++ Classes/Customer.cs
@@ -6,12 +6,27 @@ namespace Classes
     class Customer {
 
         //auto-implemented property
-        public DateTime Birthdate { get; set; }
+        public DateTime Birthdate { get; private set; }
+
+        public Customer(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+
+        }
+
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
+                return years;
+            }
+        }
         public static void Main()
         {
-            Customer c = new Customer();
-            c.Birthdate = new DateTime(2021, 1, 11);
-            Console.WriteLine(c.Birthdate);
+            Customer c = new Customer(new DateTime(2000,1,11));
+            Console.WriteLine(c.Age);
         }
 
 
