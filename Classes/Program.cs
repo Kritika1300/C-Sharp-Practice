e07214acb5c8be07092d21a2e80ede64290f4dc2
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-14 13:33:08 +0530

    Composition

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+
+    class Program
+    {
+       public static void Main()
+        {
+            DbMigrator dbmigrator = new DbMigrator(new Logger());
+            Installer installer = new Installer(new Logger());
+            dbmigrator.Migrate();
+            installer.Install();
+        }
+    }
+}
