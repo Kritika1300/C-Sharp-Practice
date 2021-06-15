c46e9e61eaf6f0986b58c9edd6a2143cb3f53ae4
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-15 18:34:59 +0530

    Multiple interface inheritance

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -8,11 +8,11 @@ namespace Classes
         void Print1();
     }
 
-    public interface ICustomer2
+    public interface ICustomer2: ICustomer1
     {
         void Print2();
     }
-    public class Customer : ICustomer1,ICustomer2
+    public class Customer : ICustomer2
     {
         public void Print1()
         {
@@ -30,6 +30,10 @@ namespace Classes
         public static void Main()
         {
             Customer c = new Customer();
+            ICustomer1 c1 = new Customer();
+            ICustomer2 c2 = new Customer(); 
+            c1.Print1(); // c1 is of type ICustomer1 so it has access to only Print1()
+            c2.Print1(); c2.Print2(); // ICustomer2 is inheriting from interface ICustomer1 thus c2 which is of type ICustomer2 can access both Print1() and Print2()
             c.Print1();
             c.Print2();
 
