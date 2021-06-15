71ecb15d9dfd7e694b6156059dbaaa7aa619637e
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-15 16:05:54 +0530

    Indexers example

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -6,13 +6,43 @@ using System.Threading.Tasks;
 
 namespace Classes
 {
-    class Program
+
+    public class Employee
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+    }
+    
+    public class Company
+    {
+        List<Employee> listofemployees;
+
+        public Company()
+        {
+             listofemployees = new List<Employee>();
+             listofemployees.Add(new Employee(){ Id = 1, Gender = "Female", Name = "Kritika"});
+             listofemployees.Add(new Employee() { Id = 2, Gender = "Male", Name = "Raj" });
+             listofemployees.Add(new Employee() { Id = 3, Gender = "Female", Name = "Kriti" });
+             listofemployees.Add(new Employee() { Id = 4, Gender = "Male", Name = "Rahul" });
+        }
+
+        public string this[int id] //indexer
+        {
+            get { return listofemployees.FirstOrDefault(emp => emp.Id == id).Name;  }
+
+            set { listofemployees.FirstOrDefault(emp => emp.Id == id).Name = value; }
+        
+        }
+
         public static void Main()
         {
-            DbMigrator dbmigrator = new DbMigrator(new FileLogger(@"C:\Users\Hp\Desktop\C#\C-Sharp-Practice\someFile.txt"));
-            dbmigrator.Migrate();
-
+            Company c = new Company();
+            c[2] = "Neha";
+            Console.WriteLine(c[3]);
         }
+
+
+
     }
 }
