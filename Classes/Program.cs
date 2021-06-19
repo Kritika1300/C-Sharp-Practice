8d858e188a48c843e45df40f16379149bdfd0c62
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-19 18:00:14 +0530

    Aggregation

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Classes
 {
     class Manager
     {
+
+        List<Workers> listofworkers = new List<Workers>(); //owner of workers can be only 1 and that is Manager in this case
         public void Enter(SwipeCard obj)
         {
             obj.Swipe(this);
@@ -14,6 +17,11 @@ namespace Classes
             Console.WriteLine("Manager name");
         }
 
+    }
+
+    class Workers
+    {
+        public string workername = "";
     }
 
     class SwipeCard
@@ -34,6 +42,7 @@ namespace Classes
         public static void Main()
         {
             Manager objManager = new Manager();
+            Workers w = new Workers(); //workers can exist independently.
          
             SwipeCard objSwipe = new SwipeCard();
 
