f5c36bdcc7c3d5cc5d51878588efd6a3cd8fef67
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-21 19:30:16 +0530

    Delegates and extensibility

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,49 +1,78 @@
 ﻿using System;
 
-
 namespace Classes
 {
-
-    public class Nullable<T> where T : struct
+    public class Photo
     {
-        private object _value;
-        public Nullable()
+        public static Photo Load(string path)
+        {
+            return new Photo();
+        }
+
+        public void Save()
         {
 
         }
-        public Nullable(T value)
-        {
-            _value = value;
-        }
+    }
 
-        public bool HasValue
-        {
-           get { return _value != null; }
-        }
+    //public class PhotoProcessor    WITHOUT USE OF DELEGATES
+    //{
+    //    public void Process(string path)
+    //    {
+    //        var photo = Photo.Load(path);
+    //        var filters = new PhotoFilters();
+    //        filters.ApplyBrightness(photo);
+    //        filters.ApplyContrast(photo);
+    //        filters.Resize(photo);
+    //        photo.Save();
+    //    }
+    //}
+    public class PhotoProcessor
+    {
+        public delegate void PhotoFilterHandler(Photo photo); // points to function with this kind of signature i.e return type void and parameter type Photo
 
-        public T GetValueOrDefault()
+        public void Process(string path,PhotoFilterHandler filterHandler)
         {
-            if (HasValue)
-            {
-                return (T)_value;
-            }
-            else
-            {
-                return default(T);
-            }
+            var photo = Photo.Load(path);
+            filterHandler(photo);
+            photo.Save();
         }
     }
 
 
+    public class PhotoFilters
+    {
+        public void ApplyBrightness(Photo photo)
+        {
+            Console.WriteLine("Applying brightness");
+        }
 
+        public void ApplyContrast(Photo photo)
+        {
+            Console.WriteLine("Applying contrast");
+        }
 
+        public void Resize(Photo photo)
+        {
+            Console.WriteLine("Resizing");
+        }
+    }
+ 
     class Program
     {
         public static void Main()
         {
-            Nullable<int> obj = new Nullable<int>(45);
-            Console.WriteLine(obj.HasValue);
-            Console.WriteLine(obj.GetValueOrDefault());
+            PhotoProcessor p = new PhotoProcessor();
+            var filters = new PhotoFilters();
+            PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBrightness;
+            filterHandler += filters.ApplyContrast;
+            filterHandler += RemoveRedEye;
+            p.Process("hewe",filterHandler);
+        }
+
+        static void RemoveRedEye(Photo photo)
+        {
+            Console.WriteLine("Remove red eye");
         }
     }
 }
