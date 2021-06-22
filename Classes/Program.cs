bd7aa96b0ede12ae3e5e3cf3784cb737c4b3ca08
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-22 11:42:35 +0530

    Delegates use case

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -3,24 +3,52 @@ using System.Collections.Generic;
 
 namespace Classes
 {
-
     class Program
     {
-
-        public delegate void HelloFunctionDelegate(string message); //A delegate that can point to a function with the same signature i.e. return type void and string parameter
-
         public static void Main()
         {
-            HelloFunctionDelegate del = new HelloFunctionDelegate(Hello); // creating instance of the delegate to make it point to a fnc.
+            List<Employee> listofemployees = new List<Employee>() 
+            { 
+                new Employee() {Id = 1, Name = "Kritika", Salary = 50000, Experience = 5},
+                new Employee() {Id = 2, Name = "Kriti", Salary = 90000, Experience = 7},
+                new Employee() {Id = 3, Name = "Roy", Salary = 70000, Experience = 8},
+                new Employee() {Id = 4, Name = "Karan", Salary = 60000, Experience = 4},
+            };
+            IsPromotable isPromotable = new IsPromotable(Promotion);
+            Employee.PromoteEmployee(listofemployees,isPromotable);
 
-            del("Hello from delegate"); // under the hood it calls the Hello function and passes it this string
-        }
-
-        public static void Hello(string message)
-        {
-            Console.WriteLine(message);
+             bool Promotion(Employee employee)
+            {
+                if (employee.Experience >= 5) return true;
+                else return false;
+            }
         }
     }
+
+    delegate bool IsPromotable(Employee employee);
+    class Employee
+    {
+        public int Id { set; get; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int Experience { get; set; }
+
+        public static void PromoteEmployee(List<Employee> listofemployees, IsPromotable IsEligibleToPromote)
+        {
+            foreach(var employee in listofemployees)
+            {
+                if(IsEligibleToPromote(employee))
+                {
+                    Console.WriteLine(employee.Name + " promoted");
+                }
+            }
+
+        }
+
+      
+    }
+
+
 
 }
 
