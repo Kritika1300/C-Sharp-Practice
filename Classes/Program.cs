b4bea8640f2e446e612bb6bd97aa0168720d5ee4
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-22 17:44:09 +0530

    Events

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,49 +1,81 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Threading;
 
 namespace Classes
 {
+
     class Program
     {
         public static void Main()
         {
-            SampleDelegate del = new SampleDelegate(Test.SampleMethodOne);
-            del += Test.SampleMethodThree;
-            del += Test.SampleMethodTwo;
-           
-            int defaultval = -1;
-            del(out defaultval);
-            Console.WriteLine(defaultval);
+            var video = new Video() { Title = "First video" };
+
+            //subscribe MailService to VideoEncoded event of VideoEncoder class
+
+            var videoEncoder = new VideoEncoder();// publisher
+            var mailService = new MailService(); //subscriber
+            var messageService = new MessageService(); //subscriber
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded; //registering event handler
+            // event has list of pointers/reference to event handlers
+            videoEncoder.VideoEncoded += messageService.OnVideoEncoded;
+                                                               
+            videoEncoder.Encode(video);
+
+          
+        }
+    }
+    public class Video
+    {
+        public string Title { get; set; }
+    }
+
+    public class VideoEncoder 
+    {
+        public delegate void VideoEncodedEventHnadler(object source, EventArgs args); //define delegate
+
+        public event VideoEncodedEventHnadler VideoEncoded; //define event
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding video.....");
+            Thread.Sleep(3000);
+
+            OnVideoEncoded(); // to notify subscribers
+            
+        }
+
+        // to raise an event we need a method -> Publisher Method
+        protected virtual void OnVideoEncoded()
+        {
+            if(VideoEncoded != null) //checks if there are subscribers
+            {
+                VideoEncoded(this, EventArgs.Empty);
+                           //who is publishing, additional info to be passed 
+            }
+
         }
     }
 
-    delegate void SampleDelegate(out int Number);
-    class Test
+    public class MailService
     {
-       public static void SampleMethodOne(out int Number)
-       {
-            Number = 1;
-       }
-
-        public static void SampleMethodTwo(out int Number)
+        public void OnVideoEncoded(object source, EventArgs e) //Event Handler
         {
-            Number = 5;
+            Console.WriteLine("Sending Mail.....");
         }
+    }
 
-        public static void SampleMethodThree(out int Number)
+    public class MessageService
+    {
+        public void OnVideoEncoded(object source, EventArgs e) //Event Handler
         {
-            Number = 6;
+            Console.WriteLine("Sending Message.....");
         }
-
-
-
-
     }
 
 
 
 }
 
-    
-        
+
+
 
