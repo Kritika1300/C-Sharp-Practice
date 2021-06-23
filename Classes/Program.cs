9756ff568b3d057b65a9a58bb6ffaadf4032144c
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-23 11:04:37 +0530

    IEnumerable extension method eg

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Classes
 {
@@ -8,34 +9,13 @@ namespace Classes
     {
         public static void Main()
         {
-            string post = "Hello this is a very very very very very very long blog post";
-            var shortenedpost = post.Shorten(10);
-            Console.WriteLine(shortenedpost);
+            IEnumerable<int> numbers = new List<int>() { 1, 28, 6, 5, 67, 0 };
+            int max = numbers.Max();
+            Console.WriteLine(max);
         }
     }
 
-    public static class StringExtensions
-    {
-        public static string Shorten(this String str,int numberOfWords)
-        {
-            if(numberOfWords < 0)
-            {
-                throw new ArgumentOutOfRangeException("Enter a number greater than or equal to zero");
-            }
-            if(numberOfWords == 0)
-            {
-                return "";
-            }
-            var words = str.Split(" ");
-
-            if(words.Length <= numberOfWords)
-            {
-                return str;
-            }
-
-            return string.Join(" ", words.Take(numberOfWords)) + "....";
-        }
-    }
+  
 
 }
 
