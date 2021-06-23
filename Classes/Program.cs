86820c7608ae61ffb9f179fc9cca05d44d81cb46
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-23 10:58:31 +0530

    Extension Methods

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,29 +1,41 @@
 ﻿using System;
-using System.Threading;
+using System.Linq;
 
 namespace Classes
 {
 
     class Program
     {
-
-        //public static bool CheckLength(string str)
-        //{
-        //    if (str.Length > 5) return true;
-        //    else return false;
-        //}
         public static void Main()
         {
-            Predicate<string> del2 = (str) =>
-            {
-                if (str.Length > 5) return true;
-                else return false;
-            };
-            //Predicate<string> del1 = CheckLength;
-            Console.WriteLine(del2.Invoke("This is a string")); 
+            string post = "Hello this is a very very very very very very long blog post";
+            var shortenedpost = post.Shorten(10);
+            Console.WriteLine(shortenedpost);
         }
     }
-   
+
+    public static class StringExtensions
+    {
+        public static string Shorten(this String str,int numberOfWords)
+        {
+            if(numberOfWords < 0)
+            {
+                throw new ArgumentOutOfRangeException("Enter a number greater than or equal to zero");
+            }
+            if(numberOfWords == 0)
+            {
+                return "";
+            }
+            var words = str.Split(" ");
+
+            if(words.Length <= numberOfWords)
+            {
+                return str;
+            }
+
+            return string.Join(" ", words.Take(numberOfWords)) + "....";
+        }
+    }
 
 }
 
