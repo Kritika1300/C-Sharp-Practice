daa0d2db9b9cc1c3212e6bdb3e94b879bc09e9f9
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-23 14:08:04 +0530

    Event handling application

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -8,21 +8,90 @@ namespace Classes
     {
         public static void Main()
         {
-            int i = 2;
-            Console.WriteLine(i.Factorial()); 
-            
+            Database db = new Database();
+            IdCardGenerator id = new IdCardGenerator();
+            EmailGenerator eg = new EmailGenerator();
+
+            UserProcessor.UserProcessorEvent += db.SaveToDb;
+            UserProcessor.UserProcessorEvent += id.GenerateIdCard;
+            UserProcessor.UserProcessorEvent += eg.GenerateEmail;
+
+            Console.WriteLine("Commands Accepted :");
+            Console.WriteLine("1. new : Add new user");
+            Console.WriteLine("2. exit : Close the Application");
+
+            while (true)
+            {
+                Console.WriteLine("\nEnter command :");
+                string cmdInput = Console.ReadLine();
+
+                if (cmdInput.Equals("exit")) break;
+                else if(cmdInput.Equals("new"))
+                {
+                    Console.WriteLine("Enter name:");
+                    var name = Console.ReadLine();
+
+                    Console.WriteLine("Enter age:");
+                    var age =int.Parse(Console.ReadLine());
+
+                    UserProcessor.ProcessUser(name, age);
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid command");
+                }
+            }
+
+
         }
     }
 
-    public static class Int32Extensions
+    public class UserProcessor
     {
-        public static int Factorial(this Int32 x)
+        public static event EventHandler<UserArgs> UserProcessorEvent;
+
+        public static void ProcessUser(string name, int age)
         {
-            if (x == 0) return 1;
-            else if (x == 1 || x == 2) return x;
-            else return x * Factorial(x - 1);
+            UserArgs args = new UserArgs();
+            args.Name = name;
+            args.Age = age;
+
+            UserProcessorEvent?.Invoke(null, args);
+
         }
     }
+
+    public class UserArgs : EventArgs
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class Database
+    {
+        public void SaveToDb(object sender, EventArgs e)
+        {
+            Console.WriteLine("Saved to db");
+        }
+    }
+
+    public class IdCardGenerator
+    {
+        public void GenerateIdCard(object sender, EventArgs e)
+        {
+            Console.WriteLine("Id card generated");
+        }
+    }
+
+    public class EmailGenerator
+    {
+        public void GenerateEmail(object sender, EventArgs e)
+        {
+            Console.WriteLine("Email generated");
+        }
+    }
+
 
 }
 
