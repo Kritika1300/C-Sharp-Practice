0ff7c93b57f8aba6502a8085a8340a2b5776a1cf
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-23 16:38:39 +0530

    Where,Order By in Linq

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Classes
 {
@@ -8,90 +9,40 @@ namespace Classes
     {
         public static void Main()
         {
-            Database db = new Database();
-            IdCardGenerator id = new IdCardGenerator();
-            EmailGenerator eg = new EmailGenerator();
 
-            UserProcessor.UserProcessorEvent += db.SaveToDb;
-            UserProcessor.UserProcessorEvent += id.GenerateIdCard;
-            UserProcessor.UserProcessorEvent += eg.GenerateEmail;
+            var newlist = new BookRepository().GetBooks();
+            var price = newlist.Where(b => b.Price > 500).OrderBy(b => b.Title);
 
-            Console.WriteLine("Commands Accepted :");
-            Console.WriteLine("1. new : Add new user");
-            Console.WriteLine("2. exit : Close the Application");
-
-            while (true)
+            foreach(var book in price)
             {
-                Console.WriteLine("\nEnter command :");
-                string cmdInput = Console.ReadLine();
-
-                if (cmdInput.Equals("exit")) break;
-                else if(cmdInput.Equals("new"))
-                {
-                    Console.WriteLine("Enter name:");
-                    var name = Console.ReadLine();
-
-                    Console.WriteLine("Enter age:");
-                    var age =int.Parse(Console.ReadLine());
-
-                    UserProcessor.ProcessUser(name, age);
-
-                }
-                else
-                {
-                    Console.WriteLine("Invalid command");
-                }
+                Console.WriteLine(book.Price + " " + book.Title);
             }
 
 
+
         }
     }
 
-    public class UserProcessor
+    public class BookRepository
     {
-        public static event EventHandler<UserArgs> UserProcessorEvent;
 
-        public static void ProcessUser(string name, int age)
+        public IEnumerable<Book> GetBooks() 
         {
-            UserArgs args = new UserArgs();
-            args.Name = name;
-            args.Age = age;
-
-            UserProcessorEvent?.Invoke(null, args);
-
-        }
-    }
-
-    public class UserArgs : EventArgs
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-    }
-
-    public class Database
-    {
-        public void SaveToDb(object sender, EventArgs e)
+           return new List<Book>()
         {
-            Console.WriteLine("Saved to db");
-        }
-    }
+            new Book() {Title = "NCERT" , Price = 200},
+            new Book() {Title = "SCARLET" , Price = 300},
+            new Book() {Title = "FLAMINGO" , Price = 200},
+            new Book() {Title = "HARRY POTTER" , Price = 700},
+            new Book() {Title = "R.D. SHARMA" , Price = 900},
+            new Book() {Title = "RS AGGARWAL" , Price = 800},
+            new Book() {Title = "CD" , Price = 600},
+        };
 
-    public class IdCardGenerator
-    {
-        public void GenerateIdCard(object sender, EventArgs e)
-        {
-            Console.WriteLine("Id card generated");
         }
+      
     }
-
-    public class EmailGenerator
-    {
-        public void GenerateEmail(object sender, EventArgs e)
-        {
-            Console.WriteLine("Email generated");
-        }
-    }
-
+      
 
 }
 
