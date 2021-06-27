7a47b28d1c546ef413da75cf7ef0a6289ae750f0
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-27 12:17:54 +0530

    Exception handling : Finally block

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,9 +7,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            StreamReader streamReader = null;
             try
             {
-                StreamReader streamReader = new StreamReader(@"D:\Persnal\C#\C-Sharp-Practice\someFile.txt");
+                streamReader = new StreamReader(@"D:\Persnal\C#\C-Sharp-Practice\ile.txt");
                 Console.WriteLine(streamReader.ReadToEnd());
             }
             catch(FileNotFoundException ex) //specific exception
@@ -22,6 +23,17 @@ namespace ConsoleApp1
             {
                 Console.WriteLine(ex.Message);
                 Console.WriteLine(ex.StackTrace);
+            }
+            finally // to release resources held by the class finallly block is used,
+                    // finally block is reqd because an exception may even occur in one the catch blocks
+                    // therefore any statements after that wouldn't be executed, so it's always better to realease resources using FINALLY (as it is guaranteed to execute)
+            {
+                if(streamReader != null)
+                {
+                    streamReader.Close();
+                }
+           
+                Console.WriteLine("Entered finally block");
             }
         }
     }
