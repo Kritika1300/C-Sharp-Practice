9bd0635296daa921427c6dd457a37e694e3628e8
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-28 11:37:03 +0530

    Preventing Exception handling abuse example

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -11,26 +11,40 @@ namespace Classes
             try
             {
                 Console.WriteLine("Enter numerator");
-                int num = Convert.ToInt32(Console.ReadLine());
+                int num;
+                bool isConversionSuccessful = Int32.TryParse(Console.ReadLine(),out num);
 
-                Console.WriteLine("Enter denominator");
-                int den = Convert.ToInt32(Console.ReadLine());
+                if(isConversionSuccessful)
+                {
+                    Console.WriteLine("Enter denominator");
+                    int den;
+                    bool isdenConversionSuccessful = Int32.TryParse(Console.ReadLine(), out den);
 
-                int result = num / den;
-                Console.WriteLine(result);
+                    if (isdenConversionSuccessful && den != 0)
+                    {
+                        int result = num / den;
+                        Console.WriteLine(result);
+                    }
+                    else
+                    {
+                        Console.WriteLine(den);
+                        if(den == 0)
+                        {
+                            Console.WriteLine("Division by zero invalid");
+                    
+                        }
+                        
+                        {
+                            Console.WriteLine("Enter a NUMBER between {0} to {1}", int.MinValue, int.MaxValue);
+                        }
+                    }
+                      
+                }
+                else
+                {
+                    Console.WriteLine("Enter a NUMBER between {0} to {1}",int.MinValue,int.MaxValue);
+                }
 
-            }
-            catch(FormatException)
-            {
-                Console.WriteLine("Enter numbers only");
-            }
-            catch (OverflowException)
-            {
-                Console.WriteLine("Enter numbers within the range : {0} to {1}",int.MinValue,int.MaxValue);
-            }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("Can't divide number by zero");
             }
             catch(Exception e)
             {
