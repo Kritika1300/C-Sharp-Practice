79735c757fa7895a5a2808d8f7f12f1ca3023f13
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-28 12:09:47 +0530

    Anonymous method

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -3,59 +3,26 @@
 
 namespace Classes
 {
-
-    class ExceptionHandling
+    public delegate string PrintHandler(string message);
+    class Program
     {
+
+        //public static string Print(string message)
+        //{
+        //   return "Hey your message is : " + message ;
+        //}
         public static void Main()
         {
-            try
+            PrintHandler obj = delegate (string message)
             {
-                Console.WriteLine("Enter numerator");
-                int num;
-                bool isConversionSuccessful = Int32.TryParse(Console.ReadLine(),out num);
-
-                if(isConversionSuccessful)
-                {
-                    Console.WriteLine("Enter denominator");
-                    int den;
-                    bool isdenConversionSuccessful = Int32.TryParse(Console.ReadLine(), out den);
-
-                    if (isdenConversionSuccessful && den != 0)
-                    {
-                        int result = num / den;
-                        Console.WriteLine(result);
-                    }
-                    else
-                    {
-                        Console.WriteLine(den);
-                        if(den == 0)
-                        {
-                            Console.WriteLine("Division by zero invalid");
-                    
-                        }
-                        
-                        {
-                            Console.WriteLine("Enter a NUMBER between {0} to {1}", int.MinValue, int.MaxValue);
-                        }
-                    }
-                      
-                }
-                else
-                {
-                    Console.WriteLine("Enter a NUMBER between {0} to {1}",int.MinValue,int.MaxValue);
-                }
-
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine("Error occured");
-            }
+                return "Hey check this message : " + message;
+            };
+            string str = obj.Invoke("Test message");
+            Console.WriteLine(str);
 
         }
+
     }
-
-    
-
 }
 
 
