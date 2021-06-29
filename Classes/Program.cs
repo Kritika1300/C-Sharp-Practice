67965a3850654a42372116cd5f1c9f57f60de39b
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-29 18:06:41 +0530

    Asynchronous execution (independent tasks)

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,44 +1,53 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Classes
 {
     class Program
     {
-
-        
-        public static void Main()
+        public static void Main(string[] args)
         {
             Calculate();
+            Console.Read();
         }
 
         public static void Calculate()
         {
-            Calculate1();
-            Calculate2();
-            Calculate3();
+            Task.Run(() =>
+            {
+                Calculate1();
+            });
+
+            Task.Run(() =>
+            {
+                Calculate2();
+            });
+
+            Task.Run(() =>
+            {
+                Calculate3();
+            });
         }
+
         public static int Calculate1()
         {
             Thread.Sleep(5000);
             Console.WriteLine("Calculate1");
             return 100;
         }
+
         public static int Calculate2()
         {
             Console.WriteLine("Calculate2");
             return 200;
         }
+
         public static int Calculate3()
         {
             Console.WriteLine("Calculate3");
             return 300;
         }
-
-
-
-
-
     }
    
 }
