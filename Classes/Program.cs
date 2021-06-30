a40a26b1fae4b49c8dd1744bf12229974206c009
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-30 20:41:06 +0530

    Linq Exercise Q1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Linq;
 using System.Collections.Generic;
-
+//www.w3resource.com/csharp-exercises/linq/csharp-linq-exercise-1.php
 namespace Classes
 {
     class Program
@@ -9,11 +9,13 @@ namespace Classes
 
         public static void Main(string[] args)
         {
-            var cheapBooks = BookList.GetBooks();
-            var cheaperBooks = from book in cheapBooks where book.Price > 10 orderby book.Price descending select book;
-            foreach(var book in cheaperBooks)
+            int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var even = from num in numbers where num % 2 == 0 select num;
+
+            foreach(var e in even)
             {
-                System.Console.WriteLine(book.Title + " " + book.Price);
+                System.Console.WriteLine(e);
             }
         }
 
