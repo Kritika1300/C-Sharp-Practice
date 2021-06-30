fa56f943f013cf8132995c0305c5a9523d2df1b1
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-30 20:44:19 +0530

    Linq Exercise Q2

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -9,13 +9,13 @@ namespace Classes
 
         public static void Main(string[] args)
         {
-            int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] numbers = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
 
-            var even = from num in numbers where num % 2 == 0 select num;
+            var positive = from num in numbers where num >= 1 && num <= 11 select num;
 
-            foreach(var e in even)
+            foreach(var p in positive)
             {
-                System.Console.WriteLine(e);
+                System.Console.WriteLine(p);
             }
         }
 
