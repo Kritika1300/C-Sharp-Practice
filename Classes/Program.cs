385bdaed4f9bb8ad4517a8fb8a7d8d06d3b8b96c
Kritika <kaur.kritika.kritika@gmail.com>
2021-06-30 20:10:51 +0530

    Linq eg

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Collections.Generic;
 
 namespace Classes
 {
@@ -10,37 +9,35 @@ namespace Classes
 
         public static void Main(string[] args)
         {
-            method2();
-            method4();
-        }
-
-        public static async void  method2()
-        {
-            await method3();
-            Console.WriteLine("Done");
-        }
-        public static async Task method3()
-        {
-
-             for (int i = 0; i <= 1000; i++)
+            var cheapBooks = BookList.GetBooks();
+            var cheaperBooks = from book in cheapBooks where book.Price > 10 orderby book.Price descending select book;
+            foreach(var book in cheaperBooks)
             {
-                Console.WriteLine("ABC");
-
-
+                System.Console.WriteLine(book.Title + " " + book.Price);
             }
-            
         }
-        public static async Task method4()
-        {
-            for(int i = 0; i <= 123; i++)
-            {
-                Console.WriteLine("1000");
 
-            }
-            
+
+
+    }
+
+    public class BookList
+    {
+        public static IEnumerable<Book> GetBooks()
+        {
+            return new List<Book>() 
+            { 
+               new Book() {Title = "NCERT" , Price = 300},
+               new Book() {Title = "ULLMAN" , Price = 500},
+               new Book() {Title = "R.S. AGGARWAL" , Price = 700},
+               new Book() {Title = "R.D. SHARMA" , Price = 600},
+               new Book() {Title = "SCARLETT" , Price = 400},
+            };
+
         }
 
     }
+
 
 }
 
