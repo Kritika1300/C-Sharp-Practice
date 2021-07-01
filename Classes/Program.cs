63ebc361ec66b3f023627ba4d33ae839b649cff0
Kritika <kaur.kritika.kritika@gmail.com>
2021-07-01 16:25:06 +0530

    Online Attendance

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -3,33 +3,69 @@
 namespace Classes
 {
 
-   
-    public delegate int MyDelegate(int a,int b);
-
-    public delegate void MyDelegate1(string message);
-    class Program
+    public class UserArgs : EventArgs 
     {
-        public static void Main()
+        public string Name { get; set; }
+    }
+    public class OnlineAttendance
+    {
+     
+        public static event EventHandler<UserArgs> AttendanceEvent;
+
+        public void MarkAttendance(string name)
         {
-        
+            OnAttendanceEvent(name);
+        }
 
-            MyDelegate del = (int a, int b) => { return a + b; };
-            int res = del(3,43);
-            Console.WriteLine(res);
-
-            MyDelegate1 del1 = delegate (string name)
-            {
-                Console.WriteLine(name);
-            };
-            del1("Kritika");
-
-          
+        protected virtual void OnAttendanceEvent(string name)
+        {
+            AttendanceEvent?.Invoke(this, new UserArgs() { Name = name });
         }
 
     }
 
+    public class FireAlarm
+    {
+        public void FireAlarmRinging(object sender, EventArgs e)
+        {
+            Console.WriteLine("Ringing the fire alarm...");
+        }
+    }
 
+    public class MailToAdmin
+    {
+        public void MailingTheAdmin(object sender, EventArgs e)
+        {
+            Console.WriteLine("Mailing the administrator......");
+        }
+    }
 
+    public class Test
+    {
+        public static void Main(string[] args)
+        {
+            OnlineAttendance oa = new OnlineAttendance();
+            MailToAdmin mail = new MailToAdmin();
+            FireAlarm alarm = new FireAlarm();
+
+            Console.WriteLine("Enter name");
+            string name = Console.ReadLine();
+
+            if(name == "Jack" || name == "Steven" || name == "Mathew")
+            {
+                OnlineAttendance.AttendanceEvent += mail.MailingTheAdmin;
+                OnlineAttendance.AttendanceEvent += alarm.FireAlarmRinging;
+
+            }
+            else
+            {
+                Console.WriteLine("hi" + name);
+            }
+
+            oa.MarkAttendance(name);
+
+        }
+    }
 
 
 }
