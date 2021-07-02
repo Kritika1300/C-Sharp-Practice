bfe6a5f584d321a92247018c897581ad8e0c0910
Kritika <kaur.kritika.kritika@gmail.com>
2021-07-02 10:31:29 +0530

    Linq Exercise Q9

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,29 +1,33 @@
 ﻿using System;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Classes
 {
 
-  //www.w3resource.com/csharp-exercises/linq/csharp-linq-exercise-7.php
+  //www.w3resource.com/csharp-exercises/linq/csharp-linq-exercise-9.php
     class Program
     {
         static void Main(string[] args)
         {
-            string[] cities =
-            {
-                "ROME","LONDON","NAIROBI","CALIFORNIA","ZURICH","NEW DELHI","AMSTERDAM","ABU DHABI", "PARIS"
-            };
+            List<int> templist = new List<int>();
+            templist.Add(55);
+            templist.Add(200);
+            templist.Add(740);
+            templist.Add(76);
+            templist.Add(230);
+            templist.Add(482);
+            templist.Add(95);
 
-            Console.WriteLine("Enter starting char");
-            char first =(char)Console.Read();
-            Console.ReadLine();
-            Console.WriteLine("Enter last char");
-            char last = (char)Console.Read();
+            var numbers = templist.Where(x => x > 80);
+            foreach (var number in numbers)
+                Console.WriteLine(number);
 
-            var res = cities.Where(x => x.StartsWith(first) && x.EndsWith(last));
-            foreach(var city in res)
-                Console.WriteLine(city);
+            Console.WriteLine();
+
+            var res = templist.FindAll(x => x > 200);
+            foreach(var number in res)
+                Console.WriteLine(number);
 
 
         }
