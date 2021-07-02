d6464183105cd44ba756b3f7f0e487c39180da84
Kritika <kaur.kritika.kritika@gmail.com>
2021-07-02 09:57:46 +0530

    Linq Exercise Q7

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -5,15 +5,17 @@ using System.Threading.Tasks;
 namespace Classes
 {
 
-  //www.w3resource.com/csharp-exercises/linq/csharp-linq-exercise-6.php
+  //www.w3resource.com/csharp-exercises/linq/csharp-linq-exercise-7.php
     class Program
     {
         static void Main(string[] args)
         {
-            string[] arr = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
-            var res = arr.Select(days => days);
-            foreach(var day in res)
-                Console.WriteLine(day);
+            int[] nums = new int[] { 5, 1, 9, 2, 3, 7, 4, 5, 6, 8, 7, 6, 3, 4, 5, 2 };
+            var res = nums.GroupBy(num => num);
+            foreach(var num in res)
+            {
+                Console.WriteLine(num.Key + " " + num.Key * num.Count() + " " + num.Count());
+            }
 
         }
 
