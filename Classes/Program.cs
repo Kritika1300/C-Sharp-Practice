81bd93c18ee856ec2a0fd22c3256914e0d91e7c5
Kritika <kaur.kritika.kritika@gmail.com>
2021-07-02 09:32:17 +0530

    Linq (getting numbers and their indexes exercise)

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -8,16 +8,15 @@ namespace Classes
   //www.w3resource.com/csharp-exercises/linq/csharp-linq-exercise-4.php
     class Program
     {
-
         static void Main(string[] args)
         {
             
 
             int[] arr1 = new int[] { 1,2,3,4,5,2,3,4,5,5,6,1 };
-            var res = arr1.GroupBy(num => num);
+            var res = arr1.Where(num => num%2 ==0).Select((num, index) => new { Number = num, Index = index });
             foreach(var i in res)
             {
-                Console.WriteLine(i.Key + " " + i.Count());
+                Console.WriteLine(i.Number + " " + i.Index);
             }
             
            
