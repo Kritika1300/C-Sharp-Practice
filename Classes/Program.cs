0b219ef06ec7c3ebcd2a0beb21ab1b292d270dfd
Kritika <kaur.kritika.kritika@gmail.com>
2021-07-07 16:26:20 +0530

    Need for sealed keyword in Singleton classes

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 namespace Classes
 {
 
-    public class Singleton 
+    public  class Singleton 
     {
         private static int counter = 0;
         private static Singleton instance = null;
@@ -30,6 +30,11 @@ namespace Classes
         {
             Console.WriteLine(message);
         }
+
+        public class DerivedSingleton : Singleton
+        {
+            //without sealed keyword DerivedSingleton can derive from Singleton class and can create new instance
+        }
     }
 
     class Program
@@ -41,6 +46,9 @@ namespace Classes
 
             Singleton fromStudent = Singleton.GetInstance;
             fromStudent.PrintDetails("Student dets");
+
+            Singleton.DerivedSingleton derived = new Singleton.DerivedSingleton();
+            derived.PrintDetails("Derived dets");
 
         }
 
