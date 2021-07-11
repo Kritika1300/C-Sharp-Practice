1f84ab583bb88d12253d27361a95ecd7e3a42d61
Kritika <kaur.kritika.kritika@gmail.com>
2021-07-11 17:33:12 +0530

    Constructor Chaining example

--- Classes/OrderProcessing.cs
+++ Classes/OrderProcessing.cs
@@ -8,37 +8,48 @@ namespace Classes
     {
         public string BrandName;
 
-        public Vehicle(string bname)
+        public string ModelName;
+
+        public Vehicle() : this("Tata","Nano")
+        {
+         
+        }
+        public Vehicle(string bname) : this(bname,"Nano")
+        {
+          
+        }
+        public Vehicle(string bname,string mname)
         {
             BrandName = bname;
+            ModelName = mname;
+
+            Console.WriteLine(BrandName + ":" + ModelName);
+         
         }
 
 
     }
-
-    class Car : Vehicle
-    {
-        public string model;
-        public Car(string m) : base("tata")  // explicit call to the base constructor is reqd if its not parameterless otherwise => compilation error.
-        {
-           
-            model = m;
-
-        }
-        
-        
-    }
-
-
     class Program
     {
         static void Main(string[] args)
         {
 
-            Car c1 = new Car("ge");
+            Vehicle c1 = new Vehicle("honda");
            
-            Console.WriteLine(c1.model + " " + c1.BrandName);
         }
     }
 
 }
+
+//class Car : Vehicle
+//{
+//    public string model;
+//    public Car(string m) : base("tata")  // explicit call to the base constructor is reqd if its not parameterless otherwise => compilation error.
+//    {
+
+//        model = m;
+
+//    }
+
+
+//}
