f13d67404a447923d776599f8b8dc654894fe2ef
Kritika <kaur.kritika.kritika@gmail.com>
2021-07-12 09:37:33 +0530

    Overriding ToString() method

--- Classes/OrderProcessing.cs
+++ Classes/OrderProcessing.cs
@@ -4,55 +4,42 @@ using System.Runtime.InteropServices;
 
 namespace Classes
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Runtime.InteropServices;
 
-    namespace Classes
-    {
-        
-        abstract class Shape
+
+    class Customer 
+    { 
+        public int Age { get; set; }
+
+        public string Name { get; set; }
+
+
+        public override string ToString() //overriding the ToString Method to return a meaningful string.
         {
-            public string Color { get; set; }
-
-            public int Size { get; set; }
-
-            public abstract void Draw();
-
-            public abstract double CalculateCost();
-
-
-        }
-
-        abstract class Circle : Shape
-        {
-
-            public override void Draw()
-            {
-                Console.WriteLine("Drawing circle.....");
-            }
-        }
-
-        class RedCircle : Circle
-        {
-            public override double CalculateCost()
-            {
-                return 3.14;
-            }
-
-
-        }
-        class Program
-        {
-            static void Main(string[] args)
-            {
-                RedCircle r = new RedCircle();
-                r.Draw();
-
-            }
+            return $"{Name} {Age}";
         }
 
     }
+
+    class Program
+        {
+            static void Main(string[] args)
+            {
+                int i = 3;
+                Console.WriteLine(i.ToString());
+
+                Customer c = new Customer();
+                c.Name = "Kritika";
+                c.Age = 21;
+                Console.WriteLine(c.ToString());
+           
+            }
+
+           
+
+
+       }
+
+    
 
 
 }
