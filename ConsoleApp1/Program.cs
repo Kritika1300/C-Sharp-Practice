b27f4c5ae3e9f7e72cd2fadd3e6c295d00948133
Kritika <kaur.kritika.kritika@gmail.com>
2021-07-13 17:09:06 +0530

    LinkedList Implementation

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,50 +4,154 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Program
+
+    class Node 
     {
-        static void Main(string[] args)
+        public int data;
+        public Node next;
+
+        public Node(int data)          
         {
-
-            Program p = new Program();
-            
-            p.OpenFacebook();
-            p.AsyncWork();
-            for (int i = 0; i < 20; i++)
-            { 
-              Console.WriteLine("Some other work");
-              Thread.Sleep(100);
-            }
-              
-            Thread.Sleep(7000);
+            this.data = data;
         }
-
-        public void OpenFacebook()
-        {
-            Console.WriteLine("Opening...");
-           
-        }
-
-        public async Task CreatePost()
-        {
-            await Task.Delay(1000);
-            Console.WriteLine("Creating Post...");
-            await Task.Delay(5000);
-            Console.WriteLine("Post Created");
-        }
-
-        public void LikePost()
-        {
-            Console.WriteLine("Liked");
-        }
-
-        public async Task AsyncWork()
-        {
-            await CreatePost();
-            
-            LikePost();
-        }
-
 
     }
+
+    class LinkedList 
+    {
+        Node head;
+
+        public void Insert(int data)
+        {
+            Node newNode = new Node(data);
+
+            if(head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node n = head;
+                while(n.next != null)
+                {
+                    n = n.next;
+                }
+                n.next = newNode;
+            }
+        }
+
+        public void InsertAtBeginning(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                newNode.next = head;
+                head = newNode;
+            }
+        }
+
+        public void InsertAtIndex(int index,int data)
+        {
+           
+            
+            Node newNode = new Node(data);
+
+            if (head == null)
+            {
+                head = newNode;
+            }
+
+            else if (index == 0)
+            {
+                InsertAtBeginning(data);
+            }
+          
+            else
+            {
+                Node n = head;
+                for (int i = 0; i < index - 1 && n != null; i++)
+                {
+                    n = n.next;
+                }
+                if(n != null)
+                {
+                    newNode.next = n.next;
+                    n.next = newNode;
+                }
+                else
+                {
+                    Insert(data);
+                }
+                
+            }
+        }
+
+        public void Traverse()
+        {
+            Node n = head;
+            while (n != null)
+            {
+                Console.Write(n.data + "->");
+                n = n.next;
+            }
+        } 
+
+        public int DeleteAtBeginning()
+        {
+            int res = head.data;
+            head = head.next;
+            return res;
+        }
+        public int DeleteAtEnd()
+        {
+            
+            Node n = head;
+            if(head == null)
+            {
+                return -1;
+            }
+            if(head.next == null)
+            {
+                int res = head.data;
+                head = null;
+                return res;
+        
+            }
+            else
+            {
+                while (n.next.next != null)
+                {
+                    n = n.next;
+                }
+                int res = n.next.data;
+                n.next = null;
+                return res;
+            }
+          
+        }
+    }
+
+    class Program
+        {
+
+            public static void Main(string[] args)
+            {
+                LinkedList l = new LinkedList();
+                l.Insert(3);
+                l.Insert(4);
+                l.Insert(5);
+                l.InsertAtBeginning(7);
+                l.InsertAtIndex(14,10);
+                Console.WriteLine(l.DeleteAtEnd());
+                l.Traverse();
+
+            }
+        }
+
+
 }
