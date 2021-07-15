84d2260d073606a3af8c01ebd38462549f5967eb
Kritika <kaur.kritika.kritika@gmail.com>
2021-07-15 12:06:26 +0530

    Named parameters

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -9,21 +9,15 @@ namespace ConsoleApp1
     public  class Test
     {
 
-        public static int Add(int a,int b, int[] arr = null)
+        public static int Add(int a,int b = 10, int c = 20)
         {
-            int result = a + b;
-            if(arr != null)
-            {
-                foreach(var i in arr) 
-                {
-                    result += i;
-                }
-            }
+            int result = a + b + c;
             return result;
         }
         static void Main()
         {
-            Console.WriteLine(Add(2,3));
+            Console.WriteLine(Add(2,c:3)); // a = 2, b = 10, c = 3
+            Console.WriteLine(Add(2,3)); // a = 2, b = 3, c = 20
         }
     }
 }
