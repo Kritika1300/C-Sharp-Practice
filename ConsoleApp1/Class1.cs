0537d6cff71c8728bed2aa951b67442e7efaef57
Kritika <kaur.kritika.kritika@gmail.com>
2021-07-17 17:34:43 +0530

    Delegates Employee Example

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -6,32 +6,23 @@ using System.Runtime.InteropServices;
 
 namespace ConsoleApp1
 {
-    public  class Test
+    interface IUser 
     {
-        [Obsolete("USE Add(List<int> list) METHOD",true)]
-        public static int Add(int a, int b)
-        {
-            return a + b;
-        }
 
+        bool Login(string username, string password);
+        bool Register(string username, string password, string email);
     
-        public static int Add(List<int> list)
-        {
-            int res = 0;
-            foreach(var i in list)
-            {
-                res += i;
-            }
-            return res;
-        }
-
-
-
-        static void Main()
-        {
-            
-            Console.WriteLine(Add(2,3));
-            Console.WriteLine(Add(new List<int>() { 1, 2, 3, 56, 6, 78, 89 }));
-        }
     }
+
+    interface ILogger
+    {
+       void LogError(string error);
+    }
+
+    interface IEmail
+    {
+        bool SendEmail(string emailContent);
+    }
+    
+    
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,8 +6,20 @@ namespace ConsoleApp1
     
     class HelloWorld
     {
-       
+        public delegate int MyDelegate(int x, int y);
+        
+        public static int Add(int a,int b)
+        {
+            return a + b;
+        }
+
+        public static void Main()
+        {
+            MyDelegate del = delegate (int x, int y) { return x + y; };
+            del(2, 3);
+        }
     }
+
 
 
 }
