4aff1e532a1834377ba3c9d4bd6eacaa0bbf1ccf
Kritika <kaur.kritika.kritika@gmail.com>
2021-07-19 19:36:33 +0530

    Dependency Inversion Principle

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,75 +3,51 @@ using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
-
-    //class OrderProcessor  A single class having 3 responsibilities...
+    //public class DataAccessLayer 
     //{
-    //    public void Validate()
+    //    public void GetCustomerNameDAL()
     //    {
-
+    //        Console.WriteLine("Name");
     //    }
-    //    public void Save()
-    //    {
-
-    //    }
-    //    public void SendNotification()
-    //    {
-
-    //    }
-
     //}
+    //public class BusinessLayer
+    //{
+    //    public void GetCustomerNameBL()
+    //    {
+    //        DataAccessLayer obj = new DataAccessLayer(); Higher level module dependent on Lower Level module
+    //        obj.GetCustomerNameDAL();
 
-    class OrderProcessor
+    //    }
+    //}
+    public class DataAccessLayer : IGetCustomerName
     {
-        private readonly ValidateOrder validateOrder;
-        private readonly SaveOrder saveOrder;
-        private readonly SendOrderNotification sendNotificationOrder;
-
-        public OrderProcessor(ValidateOrder validateOrder, SaveOrder saveOrder, SendOrderNotification sendNotificationOrder)
+        public void GetCustomerNameDAL()
         {
-            this.validateOrder = validateOrder;
-            this.saveOrder = saveOrder;
-            this.sendNotificationOrder = sendNotificationOrder;
-        }
-
-        public void Process()
-        {
-            validateOrder.Validate();
-            saveOrder.Save();
-            sendNotificationOrder.SendNotification();
-        }
-
-    }
-
-    public class ValidateOrder
-    {
-        public void Validate()
-        {
-            Console.WriteLine("Validating...");
-        }
-    }
-    public class SaveOrder
-    {
-        public void Save()
-        {
-            Console.WriteLine("Saving...");
-        }
-
-    }
-    public class SendOrderNotification
-    {
-        public void SendNotification()
-        {
-            Console.WriteLine("Sending notification....");
+            Console.WriteLine("Name");
         }
     }
 
+    public interface IGetCustomerName 
+    {
+        public void GetCustomerNameDAL();
+        
+    }
+
+    public class BusinessLayer
+    {
+        public void GetCustomerNameBL()
+        {
+            IGetCustomerName obj = new DataAccessLayer();
+            obj.GetCustomerNameDAL();
+
+        }
+    }
     public class HelloWorld
     {
         public static void Main()
         {
-            OrderProcessor o = new OrderProcessor(new ValidateOrder(),new SaveOrder(), new SendOrderNotification());
-            o.Process();
+            BusinessLayer b = new BusinessLayer();
+            b.GetCustomerNameBL();
 
         }
 
