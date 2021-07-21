1a8665878c2fafda05fe1f5a580738eccc7dc766
Kritika <kaur.kritika.kritika@gmail.com>
2021-07-21 16:34:28 +0530

    where T : interface

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -14,34 +14,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Multithreading mt = new Multithreading();
-            Console.WriteLine("Main started");
-            Thread t1 = new Thread(mt.Method1);
-            t1.Start();
-            t1.Join();
-            Console.WriteLine("t1 Ended");
-            Thread t2 = new Thread(mt.Method2);
-            t2.Start();
-            t2.Join();
-            Console.WriteLine("t2 Ended");
-            Console.WriteLine("Main Ended");
+            Compare<int> c = new Compare<int>();
+            Console.WriteLine(c.Max(5,13));
+          
         }
-
     }
-    class Multithreading
+
+    class Compare<T> where T : IComparable
     {
-        public void Method1()
+        public  T Max(T a,T b) 
         {
-            Console.WriteLine("t1 Started");
-            
+            if (a.CompareTo(b) > 0) return a;
+            else return b;
         }
-
-        public void Method2()
-        {
-            Console.WriteLine("t2 Started");
-        }
-
     }
+   
 
    
 
