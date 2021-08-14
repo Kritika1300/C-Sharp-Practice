7762438adde14ee0c67a642d5e2cb2dddf283c7d
Kritika <kaur.kritika.kritika@gmail.com>
2021-08-14 22:54:10 +0530

    Mulithreading

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,32 +1,57 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
 
 namespace ConsoleProgram
 {
 
     class Program
     {
-        
-        delegate int AdditionDelegate(int a, int b); // Delegate definition
-
-        public static int Add(int a,int b) // the function to which AdditionDelegate points
-        {
-            return a + b;
-        }
-
         public static void Main()
         {
-            AdditionDelegate ad = Program.Add; // Instantiation
-            int res = ad(3,4); // Invocation
-            Console.WriteLine(res);
-            ad = Subtract; // Compile time error
+            Thread t1 = new Thread(Task1);
+            Thread t2 = new Thread(Task2);
+            Thread t3 = new Thread(Task3);
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+       
         }
 
-        // delegate is type safe function pointer
-        public static void Subtract(int a, int b)
+        public static void Task1()
         {
-            Console.WriteLine(a - b);
+           Console.WriteLine("Task1 started using Thread t1");
+           for(int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Task1 " + i);
+            }
+            Console.WriteLine("Task1 ended using Thread t1");
         }
+
+        public static void Task2()
+        {
+            Console.WriteLine("Time consuming task started using Thread t2");
+            for (int i = 1; i <=5 ; i++)
+            {
+                Console.WriteLine("Task2 " + i);
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("Time consuming task ended using Thread t2");
+        }
+
+        public static void Task3()
+        {
+            Console.WriteLine("Task3 started using Thread t3");
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Task3 " + i);
+                Thread.Sleep(50);
+            }
+            Console.WriteLine("Task3 ended using Thread t3");
+        }
+
+        
 
     }
 }
